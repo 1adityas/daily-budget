224c28a65fbe680dd205f4710a1fe1c2f73abfc5
aditya pratap singh <1adityapsingh@gmail.com>
2022-10-30 15:30:41 +0530

    background changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Header from "./components/header";
+import "./global-css.css";
+import bgVideo from "./assets/bgVideo.mp4";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainBody">
+      <video src={bgVideo} autoPlay loop muted></video>
+      <div className="mainHeader">
+        {" "}
+        <div className="bodyFlex">
+          <Header></Header>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../global-css.css";
+
+function Header() {
+  return (
+    <div>
+      <div className="headerBoi"></div>
+      <h1>yoooo</h1>
+    </div>
+  );
+}
+
+export default Header;
