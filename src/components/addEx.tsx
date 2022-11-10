1433e23ba3c87e693ab164780229471d523e3a95
aditya <aditya.s@outlook.com>
2022-11-10 16:37:02 +0530

    new time extension

--- src/components/addEx.tsx
+++ src/components/addEx.tsx
@@ -1,12 +1,19 @@
 import { HtmlHTMLAttributes, useRef } from "react";
+import ReactDOM from 'react-dom';
+
 
 export default function AddEx() {
   const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className="exCard">
       <label>
         <input className="addExText" ref={inputRef}></input>
-        <button id="add" onClick={(e) => {console.log(inputRef.current?.value)}}>add</button>
+        <button id="add" onClick={(e) => { 
+          let tim=new Date().toLocaleTimeString();
+          console.log(inputRef.current?.value);
+          console.log(tim);
+           }}>add</button>
       </label>
     </div>
   );
